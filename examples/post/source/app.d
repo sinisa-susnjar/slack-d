f2308b1c7d57bf995739e6701389de6d556da8c5
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-10-09 16:46:29 +0100

    some experiments

--- examples/post/source/app.d
+++ examples/post/source/app.d
@@ -13,9 +13,12 @@ void main()
 	writeln("json: %s", toJSON(json, true));
 	}
 
-	auto token = "xoxp-533837141921-535502563895-551343694551-8448aff7fdde6771682d00c851f7bea3";
+	// auto token = "xoxp-533837141921-535502563895-551343694551-8448aff7fdde6771682d00c851f7bea3";
+	auto token = "xoxp-2571722380439-2588608190228-2571735157735-8d6d27ca02ca25e497a1d76ae68484bf";
+	auto bot_token = "xoxb-2571722380439-2586427320738-CRtXjaHr41Wr66GWBqB2TLtX";
 	auto url = "https://slack.com/api/";
-	auto channel = "#DEMO";
+	// auto channel = "#DEMO";
+	auto channel = "#random";
 
 	// auto http = HTTP();
 	// http.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -23,11 +26,34 @@ void main()
 	// auto content = post(url ~ "conversations.list", `{{"token":"` ~ token ~ `"}}`, http);
 	// writeln("content: %s", content);
 
+	{
 	auto http = HTTP();
 	http.addRequestHeader("Content-Type", "application/json");
-	http.addRequestHeader("Authorization", "Bearer " ~ token);
+	http.addRequestHeader("Authorization", "Bearer " ~ bot_token);
 	auto content = post(url ~ "chat.postMessage",
 				`{"channel":"` ~ channel ~ `", "attachments": [{"text":"Hello from slack-d!","color":"good"}]}`,
 				http);
 	writeln("content: %s", content);
+	}
+
+
+	{
+	auto http = HTTP();
+	http.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	auto content = post(url ~ "conversations.list", ["token": bot_token, "exclude_archived": "true"], http);
+	// auto content = post(url ~ "conversations.list", `{{"token":"` ~ token ~ `"}}`, http);
+	auto json = parseJSON(content);
+	writeln("channels: %s", toJSON(json, true));
+	}
+
+	{
+	auto http = HTTP();
+	http.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	auto content = post(url ~ "conversations.history",
+						["token": bot_token, "channel": "C02HAHW5SNQ"],
+						http);
+	// auto content = post(url ~ "conversations.list", `{{"token":"` ~ token ~ `"}}`, http);
+	auto json = parseJSON(content);
+	writeln("history: %s", toJSON(json, true));
+	}
 }
