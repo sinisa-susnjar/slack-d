2a816c011d8c676ea61866c0d1733877e83df6e3
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2021-10-10 00:03:54 +0100

    got rid of hard-coded token

--- examples/post/source/app.d
+++ examples/post/source/app.d
@@ -1,59 +1,34 @@
 import std.stdio;
+import std.process;
 import slack;
 
-import std.net.curl;
-import std.json;
-
-void main()
-{
-	version (None) {
-	auto content = post("https://httpbin.org/post", ["name1" : "value1", "name2" : "value2"]);
-	writeln("content: %s", content);
-	auto json = parseJSON(content);
-	writeln("json: %s", toJSON(json, true));
+int main() {
+	auto token = environment.get("SLACK_TOKEN");
+	if (token is null) {
+		writefln("Please set the environment variable SLACK_TOKEN");
+		return 1;
 	}
 
-	// auto token = "xoxp-533837141921-535502563895-551343694551-8448aff7fdde6771682d00c851f7bea3";
-	auto token = "xoxp-2571722380439-2588608190228-2571735157735-8d6d27ca02ca25e497a1d76ae68484bf";
-	auto bot_token = "xoxb-2571722380439-2586427320738-CRtXjaHr41Wr66GWBqB2TLtX";
-	auto url = "https://slack.com/api/";
-	// auto channel = "#DEMO";
-	auto channel = "#random";
+	auto slack = Slack(token);
 
-	// auto http = HTTP();
-	// http.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	// auto content = post(url ~ "conversations.list", ["token": token, "exclude_archived": "true"], http);
-	// auto content = post(url ~ "conversations.list", `{{"token":"` ~ token ~ `"}}`, http);
-	// writeln("content: %s", content);
+	/*
+	auto r = slack.postMessage("Hello from slack-d!");
+	if (r)
+		writefln("posted message to channel %s", slack.channel);
+	else
+		writefln("Oops - failed to post to %s: %s", slack.channel, r);
+	*/
 
-	{
-	auto http = HTTP();
-	http.addRequestHeader("Content-Type", "application/json");
-	http.addRequestHeader("Authorization", "Bearer " ~ bot_token);
-	auto content = post(url ~ "chat.postMessage",
-				`{"channel":"` ~ channel ~ `", "attachments": [{"text":"Hello from slack-d!","color":"good"}]}`,
-				http);
-	writeln("content: %s", content);
+	auto r = slack.conversationsList();
+	if (!r) {
+		writefln("failed to get conversations list: %s", r);
+		return 1;
 	}
 
-
-	{
-	auto http = HTTP();
-	http.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	auto content = post(url ~ "conversations.list", ["token": bot_token, "exclude_archived": "true"], http);
-	// auto content = post(url ~ "conversations.list", `{{"token":"` ~ token ~ `"}}`, http);
-	auto json = parseJSON(content);
-	writeln("channels: %s", toJSON(json, true));
+	r = slack.conversationsHistory("C02HAHW5SNQ-xxx");
+	if (!r) {
+		writefln("failed to get conversations history: %s", r);
+		return 1;
 	}
-
-	{
-	auto http = HTTP();
-	http.addRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	auto content = post(url ~ "conversations.history",
-						["token": bot_token, "channel": "C02HAHW5SNQ"],
-						http);
-	// auto content = post(url ~ "conversations.list", `{{"token":"` ~ token ~ `"}}`, http);
-	auto json = parseJSON(content);
-	writeln("history: %s", toJSON(json, true));
-	}
+	return 0;
 }
