8576901c5f2521213607dfb3d7c55eb592236559
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2022-09-05 23:59:15 +0200

    rather use SLACK_TOKEN env too

--- examples/simple/source/app.d
+++ examples/simple/source/app.d
@@ -1,9 +1,12 @@
-import std.stdio;
+import std.process, std.stdio;
 import slack;
 
 void main() {
+	auto token = environment.get("SLACK_TOKEN");
+	assert(token != null, "Please set the SLACK_TOKEN environment variable");
+
 	// Create a Slack object using the given token.
-	auto slack = Slack("xoxb-YOUR-BOT-TOKEN-HERE");
+	auto slack = Slack(token);
 
 	// Post a message.
 	auto r = slack.postMessage("Hello from slack-d !");
