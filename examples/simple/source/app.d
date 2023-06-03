77d8f81fe6dfbac785631a08baeba901aa97e5d9
Sinisa Susnjar <sinisa.susnjar@gmail.com>
2023-06-04 00:45:09 +0200

    async test

--- examples/simple/source/app.d
+++ examples/simple/source/app.d
@@ -1,4 +1,6 @@
+import std.datetime.systime : SysTime, Clock;
 import std.process, std.stdio;
+import std.conv : to;
 import slack;
 
 void main() {
@@ -8,7 +10,21 @@ void main() {
 	// Create a Slack object using the given token.
 	auto slack = Slack(token);
 
-	// Post a message.
-	auto r = slack.postMessage("Hello from slack-d !");
-	writeln(r);
+	writeln("Post 10 synchronous messages.");
+    Response[] r1;
+    auto t = Clock.currTime();
+    foreach (n; 0..10)
+	    r1 ~= slack.postMessage(to!string(n) ~ " sync hello from slack-d !", false);
+    auto d = Clock.currTime() - t;
+    writefln("Took: %s", d);
+    // foreach (ref r; r1) writeln(r);
+
+	writeln("Post 10 asynchronous messages.");
+    Response[10] r2;
+    t = Clock.currTime();
+    foreach (n; 0..10)
+	    r2[n] = slack.postMessage(to!string(n) ~ " async hello from slack-d !", true);
+    d = Clock.currTime() - t;
+    writefln("Took: %s", d);
+    // foreach (ref r; r2) writeln(r.await);
 }
